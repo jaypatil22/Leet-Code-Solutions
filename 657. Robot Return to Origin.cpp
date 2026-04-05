2170f4e7a248b9f3e23163d901a805f3c1dc83df
Vijay Patil <patilvjay22@gmail.com>
2026-04-06 00:47:48 +0530

    657. Robot Return to Origin

---
+++ 657. Robot Return to Origin.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int row=0,col=0;
+        for(int i=0;i<moves.length();i++) {
+            if(moves[i]=='L') col--;
+            else if(moves[i]=='R') col++;
+            else if(moves[i]=='U') row--;
+            else row++;
+        }
+        return (row==0) && (col==0);
+    }
+};
